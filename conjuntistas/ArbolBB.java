f8fe84fa59a5ea59c1f8d16761ca9206b3ee1212
simonVillarroel <111023669+simonVillarroel@users.noreply.github.com>
2024-06-06 12:48:59 -0300

    AC

--- conjuntistas/ArbolBB.java
+++ conjuntistas/ArbolBB.java
@@ -26,77 +26,77 @@ public class ArbolBB {
             } else {
                 if (buscado.compareTo(nodo.getElem()) < 0) {
                     exito = perteneceAux(nodo.getIzquierdo(), buscado);
-                } else if(buscado.compareTo(nodo.getElem()) > 0){
+                } else if (buscado.compareTo(nodo.getElem()) > 0) {
                     exito = perteneceAux(nodo.getDerecho(), buscado);
                 }
             }
         }
         return exito;
     }
-    
-    public boolean insertar(Comparable elem){
+
+    public boolean insertar(Comparable elem) {
         boolean exito = true;
-        if(this.raiz == null){
+        if (this.raiz == null) {
             this.raiz = new NodoABB(elem);
-        }else{
+        } else {
             exito = insertarAux(this.raiz, elem);
         }
         return exito;
     }
-    
-    private boolean insertarAux(NodoABB nodo, Comparable elem){
+
+    private boolean insertarAux(NodoABB nodo, Comparable elem) {
         //precondicion: n no es nulo
         boolean exito = true;
-        if(elem.compareTo(nodo.getElem()) == 0){
+        if (elem.compareTo(nodo.getElem()) == 0) {
             //elemento repetido
-           exito = false;
-        }else if(elem.compareTo(nodo.getElem()) < 0){
+            exito = false;
+        } else if (elem.compareTo(nodo.getElem()) < 0) {
             //elem es menor que nodo.getElem()
             //si tiene HI baja a la izquierda, sino agrega elem
-            if(nodo.getIzquierdo() == null){
+            if (nodo.getIzquierdo() == null) {
                 nodo.setIzquierdo(new NodoABB(elem));
-            }else{
+            } else {
                 exito = insertarAux(nodo.getIzquierdo(), elem);
             }
-        }else{
+        } else {
             //elem es mayor que nodo.getElem()
             //si tiene HD baja a la derecha, sino agrega elem
-            if(nodo.getDerecho() == null){
+            if (nodo.getDerecho() == null) {
                 nodo.setDerecho(new NodoABB(elem));
-            }else{
+            } else {
                 exito = insertarAux(nodo.getDerecho(), elem);
             }
         }
         return exito;
     }
 
-    public boolean eliminar(Comparable elem){
+    public boolean eliminar(Comparable elem) {
         boolean exito = true;
-        exito = eliminarAux(this.raiz, null, elem);            
+        exito = eliminarAux(this.raiz, null, elem);
         return exito;
     }
 
-    private boolean eliminarAux(NodoABB nodoActual, NodoABB nodoPadre, Comparable elem){
+    private boolean eliminarAux(NodoABB nodoActual, NodoABB nodoPadre, Comparable elem) {
         boolean exito = true;
-        if(nodoActual == null){
+        if (nodoActual == null) {
             exito = false;
-        }else{
-            if(elem.compareTo(nodoActual.getElem()) < 0){
+        } else {
+            if (elem.compareTo(nodoActual.getElem()) < 0) {
                 exito = eliminarAux(nodoActual.getIzquierdo(), nodoActual, elem);
-            }else if(elem.compareTo(nodoActual.getElem()) > 0){
+            } else if (elem.compareTo(nodoActual.getElem()) > 0) {
                 exito = eliminarAux(nodoActual.getDerecho(), nodoActual, elem);
-            }else{
+            } else {
                 //se encontro el elemento a eliminar
                 NodoABB hijoIzq, hijoDer;
                 hijoIzq = nodoActual.getIzquierdo();
                 hijoDer = nodoActual.getDerecho();
-                if(hijoIzq == null && hijoDer == null){
+                if (hijoIzq == null && hijoDer == null) {
                     //no tiene hijos: borrar segun caso 1
                     eliminarCaso1(nodoActual, nodoPadre);
-                }else if((hijoIzq != null && hijoDer == null) || (hijoIzq == null && hijoDer != null)){
+                } else if ((hijoIzq != null && hijoDer == null) || (hijoIzq == null && hijoDer != null)) {
                     //tiene al menos un hijo: borrar segun caso 2
                     eliminarCaso2(nodoActual, nodoPadre);
-                }else{
+                } else {
                     //tiene ambos hijos: borrar segun caso 3
                     eliminarCaso3(nodoActual, nodoPadre);
                 }
@@ -105,85 +105,85 @@ public class ArbolBB {
         return exito;
     }
 
-    private void eliminarCaso1(NodoABB nodoActual, NodoABB nodoPadre){
-        if(nodoActual == this.raiz){
+    private void eliminarCaso1(NodoABB nodoActual, NodoABB nodoPadre) {
+        if (nodoActual == this.raiz) {
             this.raiz = null;
-        }else{
-            if(nodoActual.getElem().compareTo(nodoPadre.getElem()) < 0){
+        } else {
+            if (nodoActual.getElem().compareTo(nodoPadre.getElem()) < 0) {
                 //el nodo a eliminar es HI de nodoPadre
                 nodoPadre.setIzquierdo(null);
-            }else{
+            } else {
                 //el nodo a eliminar es HD de nodoPadre
                 nodoPadre.setDerecho(null);
             }
         }
     }
 
-    private void eliminarCaso2(NodoABB nodoActual, NodoABB nodoPadre){
-        if(nodoActual.getElem().compareTo(nodoPadre.getElem()) < 0){
+    private void eliminarCaso2(NodoABB nodoActual, NodoABB nodoPadre) {
+        if (nodoActual.getElem().compareTo(nodoPadre.getElem()) < 0) {
             //el nodo a eliminar es HI de nodoPadre
-            if(nodoActual.getIzquierdo() != null){
+            if (nodoActual.getIzquierdo() != null) {
                 //el HI de nodoActual pasa a ser HI de nodoPadre
                 nodoPadre.setIzquierdo(nodoActual.getIzquierdo());
-            }else{
+            } else {
                 //el HD de nodoActual pasa a ser HI de nodoPadre
                 nodoPadre.setIzquierdo(nodoActual.getDerecho());
             }
-        }else{
+        } else {
             //el nodo a eliminar es HD de nodoPadre
-            if(nodoActual.getIzquierdo() != null){
+            if (nodoActual.getIzquierdo() != null) {
                 //el HI de nodoActual pasa a ser HD de nodoPadre
                 nodoPadre.setDerecho(nodoActual.getIzquierdo());
-            }else{
+            } else {
                 //el HD de nodoActual pasa a ser HD de nodoPadre
                 nodoPadre.setDerecho(nodoActual.getDerecho());
             }
         }
     }
 
-    private void eliminarCaso3(NodoABB nodoActual, NodoABB nodoPadre){
-        if(nodoActual.getIzquierdo() != null){
+    private void eliminarCaso3(NodoABB nodoActual, NodoABB nodoPadre) {
+        if (nodoActual.getIzquierdo() != null) {
             nodoActual.setElem(mejorCandidatoA(nodoActual, nodoPadre));
-        }else{
-            nodoActual.setElem(mejorCandidatoB(nodoActual, nodoPadre));        
+        } else {
+            nodoActual.setElem(mejorCandidatoB(nodoActual, nodoPadre));
         }
     }
 
-    private Comparable mejorCandidatoA(NodoABB nodoActual, NodoABB nodoPadre){
+    private Comparable mejorCandidatoA(NodoABB nodoActual, NodoABB nodoPadre) {
         //busca el mayor elemento del subarbol izquierdo del nodoActual
         Comparable candidato;
         nodoPadre = nodoActual;
         nodoActual = nodoActual.getIzquierdo();//me muevo por unica vez al subarbol izquierdo
-        while(nodoActual.getDerecho() != null){//busco el extremo derecho del subarbol derecho
+        while (nodoActual.getDerecho() != null) {//busco el extremo derecho del subarbol derecho
             nodoPadre = nodoActual;
             nodoActual = nodoActual.getDerecho();
         }
         candidato = nodoActual.getElem();
-        
-        
-        if(nodoActual.getIzquierdo() != null){
+
+        if (nodoActual.getIzquierdo() != null) {
             //el candidato tiene HI: se debe eliminar segun caso 2
             nodoPadre.setDerecho(nodoActual.getIzquierdo());
-        }else{
+        } else {
             //el candidato no tiene hijos: se debe eliminar segun caso 1
             nodoPadre.setDerecho(null);
         }
         return candidato;
     }
-    private Comparable mejorCandidatoB(NodoABB nodoActual, NodoABB nodoPadre){
+
+    private Comparable mejorCandidatoB(NodoABB nodoActual, NodoABB nodoPadre) {
         //busca el menor elemento del subarbol derecho del nodoActual
         Comparable candidato;
         nodoPadre = nodoActual;
         nodoActual = nodoActual.getDerecho();//me muevo por unica vez al subarbol derecho
-        while(nodoActual.getIzquierdo() != null){//busco el extremo izquierdo del subarbol izquierdo
+        while (nodoActual.getIzquierdo() != null) {//busco el extremo izquierdo del subarbol izquierdo
             nodoPadre = nodoActual;
             nodoActual = nodoActual.getIzquierdo();
         }
         candidato = nodoActual.getElem();
-        if(nodoActual.getDerecho() != null){
+        if (nodoActual.getDerecho() != null) {
             //el candidato tiene HD: se debe eliminar segun caso 2
             nodoPadre.setIzquierdo((nodoActual.getDerecho()));
-        }else{
+        } else {
             //el candidato no tiene hijos: se debe eliminar segun caso 1
             nodoPadre.setIzquierdo(null);
         }
@@ -199,7 +199,7 @@ public class ArbolBB {
         }
         return minimo;
     }
-    
+
     public Comparable maximoElem() {
         NodoABB nodo = this.raiz;
         Comparable maximo = null;
@@ -210,65 +210,65 @@ public class ArbolBB {
         return maximo;
     }
 
-    public boolean esVacio(){
+    public boolean esVacio() {
         return this.raiz == null;
     }
 
-    public Lista listar(){
+    public Lista listar() {
         Lista lista = new Lista();
-        if(this.raiz != null){
+        if (this.raiz != null) {
             listarAux(this.raiz, lista);
         }
         return lista;
     }
 
-    private void listarAux(NodoABB nodo, Lista lista){
-        if(nodo != null){
+    private void listarAux(NodoABB nodo, Lista lista) {
+        if (nodo != null) {
             listarAux(nodo.getIzquierdo(), lista);
-            lista.insertar(nodo.getElem(), lista.longitud()+1);
+            lista.insertar(nodo.getElem(), lista.longitud() + 1);
             listarAux(nodo.getDerecho(), lista);
         }
     }
 
-    public Lista listarRango(Comparable minimo, Comparable maximo){
+    public Lista listarRango(Comparable minimo, Comparable maximo) {
         Lista lista = new Lista();
-        if(this.raiz != null){
+        if (this.raiz != null) {
             listarRangoAux(this.raiz, lista, minimo, maximo);
         }
         return lista;
     }
 
-    private void listarRangoAux(NodoABB nodo, Lista lista, Comparable min, Comparable max){
-        if(nodo != null){
+    private void listarRangoAux(NodoABB nodo, Lista lista, Comparable min, Comparable max) {
+        if (nodo != null) {
             System.out.println("entre con nodo " + nodo.getElem());
             int mayorQueMinimo = nodo.getElem().compareTo(min);
             int menorQueMaximo = nodo.getElem().compareTo(max);
-            
+
             listarRangoAux(nodo.getIzquierdo(), lista, min, max);
-            
-            if(mayorQueMinimo >= 0 && menorQueMaximo <= 0){
+
+            if (mayorQueMinimo >= 0 && menorQueMaximo <= 0) {
                 //el elemento a insertar esta dentro de los limites o es igual a uno de estos
-                lista.insertar(nodo.getElem(), lista.longitud()+1);
+                lista.insertar(nodo.getElem(), lista.longitud() + 1);
             }
-            if(nodo.getElem().compareTo(max) < 0){
+            if (nodo.getElem().compareTo(max) < 0) {
                 listarRangoAux(nodo.getDerecho(), lista, min, max);
             }
         }
     }
 
-    public void vaciar(){
+    public void vaciar() {
         this.raiz = null;
     }
-    
-    public ArbolBB clone(){
+
+    public ArbolBB clone() {
         ArbolBB clon = new ArbolBB();
         clon.raiz = cloneAux(this.raiz);
         return clon;
     }
 
-    private NodoABB cloneAux(NodoABB nodo){
+    private NodoABB cloneAux(NodoABB nodo) {
         NodoABB nodoClon = null;
-        if(nodo != null){
+        if (nodo != null) {
             nodoClon = new NodoABB(nodo.getElem());
             nodoClon.setIzquierdo(cloneAux(nodo.getIzquierdo()));
             nodoClon.setDerecho(cloneAux(nodo.getDerecho()));
@@ -308,33 +308,65 @@ public class ArbolBB {
         }
         return cadena;
     }
-    
-    public boolean eliminarMinimo(){
+
+    //////////EJERCICIOS ADICIONALES//////////
+    public boolean eliminarMinimo() {
         boolean exito = false;
-        if(this.raiz.getIzquierdo() != null){
-            exito = eliminarMinimoAux(this.raiz.getIzquierdo(), this.raiz);    
-        }else{
+        if (this.raiz.getIzquierdo() != null) {
+            exito = eliminarMinimoAux(this.raiz.getIzquierdo(), this.raiz);
+        } else {
             this.raiz = this.raiz.getDerecho();
             exito = true;
         }
-        
+
         return exito;
     }
-    private boolean eliminarMinimoAux(NodoABB nodo, NodoABB nodoPadre){
+
+    private boolean eliminarMinimoAux(NodoABB nodo, NodoABB nodoPadre) {
         boolean exito = false;
-        if(nodo != null){
-            System.out.println("nodo: "+nodo.getElem());
-            if(nodo.getIzquierdo() != null){
+        if (nodo != null) {
+            System.out.println("nodo: " + nodo.getElem());
+            if (nodo.getIzquierdo() != null) {
                 exito = eliminarMinimoAux(nodo.getIzquierdo(), nodo);
-            }else{
-                if(nodo.getDerecho() == null){
+            } else {
+                if (nodo.getDerecho() == null) {
                     nodoPadre.setIzquierdo(null);
-                }else{
+                } else {
                     nodoPadre.setIzquierdo(nodo.getDerecho());
                 }
                 exito = true;
             }
         }
         return exito;
+    }
+
+    public ArbolBB clonarParteInvertida(Comparable elem) {
+        ArbolBB clonInvertido = new ArbolBB();
+        clonInvertido.raiz = parteInvertida(this.raiz, elem);
+        return clonInvertido;
+    }
+
+    private NodoABB parteInvertida(NodoABB nodo, Comparable elem) {
+        NodoABB nodoClon = null;
+        if (nodo != null) {
+            if (elem.compareTo(nodo.getElem()) == 0) {
+                nodoClon = invertir(nodo);
+            }else if (elem.compareTo(nodo.getElem()) < 0) {
+                    nodoClon = parteInvertida(nodo.getIzquierdo(), elem);
+                } else {
+                    nodoClon = parteInvertida(nodo.getDerecho(), elem);
+                }
+        }
+        return nodoClon;
+    }
+    
+    private NodoABB invertir(NodoABB nodo){
+        NodoABB nodoClon = null;
+        if(nodo != null){
+            nodoClon = new NodoABB(nodo.getElem());
+            nodoClon.setIzquierdo(invertir(nodo.getDerecho()));
+            nodoClon.setDerecho(invertir(nodo.getIzquierdo()));
+        }
+        return nodoClon;
     }
 }
